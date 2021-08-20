8c3653695656d6267da1240138ee0e125e7df9af
timur <you@example.com>
2021-08-20 22:29:58 +0300

    bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+A = [25, 1, 19, 22, 9, 18, 30, 24, 34, 25, 49, 15, 13, 10, 1, 0, 32, 6, 40, 34]
+
+
+def bubble_sort(sort_list: list) -> list:
+    array_list = len(sort_list) - 1
+    for i in range(array_list):
+        for j in range(array_list - i):
+            if sort_list[j] > sort_list[j + 1]:
+                sort_list[j], sort_list[j + 1] = sort_list[j + 1], sort_list[j]
+    return sort_list
+
+
+print(bubble_sort(A))
