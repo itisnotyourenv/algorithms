1ea67381c2862c0c2a5a57b4cbe1207017ed8bea
timur <you@example.com>
2021-08-31 03:00:03 +0300

    Created wrapper for count the time

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,9 @@
+from main import func_timer
+
 A = [25, 1, 19, 22, 9, 18, 30, 24, 34, 25, 49, 15, 13, 10, 1, 0, 32, 6, 40, 34]
 
 
+@func_timer
 def bubble_sort(sort_list: list) -> list:
     array_list = len(sort_list) - 1
     for i in range(array_list):
---
+++ main.py
@@ -0,0 +1,9 @@
+from time import time
+
+def func_timer(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        res = func(*args, **kwargs)
+        print(time() - start)
+        return res
+    return wrapper
